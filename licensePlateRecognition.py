fcc4966d998863448c3422245de927222bbbf201
Mikayla Campbell <campbell.mikaylaj@gmail.com>
2023-02-26 13:37:12 -0800

    Removing BFMatcher

--- licensePlateRecognition.py
+++ licensePlateRecognition.py
@@ -11,7 +11,7 @@ def buildTemplates():
     return state_templates# , letter_number_templates
 
 
-def findState(img, state_templates):
+def findMatches(img, state_templates):
     img_features = st.extractDesignFeatures(img)
 
     bf = cv2.BFMatcher()
@@ -29,6 +29,55 @@ def findState(img, state_templates):
                 verified_matches.append([match1])
         match_size = len(verified_matches)
         state = curr_state
+        matches_list.append(match_size)
+        if match_size > 60:
+            loop_broken = 1
+            break
+
+    if loop_broken == 0:
+        match_size = max(matches_list)
+        state = st.states[matches_list.index(match_size)]
+
+    return state
+
+
+def findState(img, state_templates):
+    img_features = st.extractDesignFeatures(img)
+    match_size = 0
+    state = ''
+    matches_list = []
+    loop_broken = 0
+    
+    for i in range(len(state_templates)):
+        keypoint_matches = {}
+        curr_template = state_templates[st.states[i]]
+        for j in range(len(curr_template[1])):
+            dists = []
+            dist_dict = {}
+            matches = []
+            for k in range(len(img_features[1])):
+                dist = np.linalg.norm(curr_template[1][j] - img_features[1][k])
+                dists.append(dist)
+                dist_dict[dist] = [img_features[0][k].pt[0], img_features[0][k].pt[1], dist]
+
+            # add two shortest distances to matches
+            matches.append(dist_dict[min(dists)])
+            dists.remove(min(dists))
+            matches.append(dist_dict[min(dists)])
+            keypoint_matches[curr_template[0][j]] = matches
+
+        # ratio test
+        list_pairs_matched_keypoints = []
+        for point in curr_template[0]:
+            p2 = keypoint_matches[point][0]
+            p3 = keypoint_matches[point][1]
+            if p2[2] < p3[2] * 0.7:
+                list_pairs_matched_keypoints.append(
+                    [[point.pt[0], point.pt[1]], [p2[0], p2[1]]])
+
+
+        match_size = len(list_pairs_matched_keypoints)
+        state = st.states[i]
         matches_list.append(match_size)
         if match_size > 60:
             loop_broken = 1
