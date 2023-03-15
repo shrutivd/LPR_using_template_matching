94d3791b3a7b53add923a445235471b671167adb
Mikayla Campbell <campbell.mikaylaj@gmail.com>
2023-03-15 14:00:38 -0700

    Cleaning up unneeded files

--- newDriver.py
+++ newDriver.py
@@ -1,6 +1,6 @@
 def registerNewDriver(state, lp, fname, lname, email, number, bday):
     new_driver = str(state) + "," + str(lp) + "," + str(fname) \
-        + "," + str(lname) + "," + str(email) + "," + \
+        + " " + str(lname) + "," + str(email) + "," + \
         str(number) + "," + str(bday) + "\n"
 
     with open("driverinfo.csv", "a") as zh:
